995017e9c0c63808790d44a1c77ea8c9beeb8ec4
TomasBianchini <tomassbianchini@gmail.com>
2024-09-26 11:42:16 -0300

    Add triangle function and package.json

---
+++ index.js
@@ -0,0 +1,20 @@
+export function triangle(a, b, c) {
+    if(Number.isNaN(a) || Number.isNaN(b) || Number.isNaN(c)) {
+        return 'Tipo de dato incorrecto';
+    }
+    if (a <= 0 || b <= 0 || c <= 0) {
+        return 'Tipo de dato incorrecto';
+    }
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        return  'No es un triangulo';
+    }
+
+    if (a === b && b === c) {
+        return 'Equilatero';
+    } else if (a === b || b === c || a === c) {
+        return 'Isosceles';
+    } else {
+        return 'Escaleno';
+    }
+
+}
